8981e810d10ded8e758243e4b3e5ed463c3b257b
Denis Januzi <denisjanuzi@gmail.com>
2024-06-24 09:48:47 +0200

    update

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import '../styles/normalize.css';
 import '../styles/general.css';
 import '../styles/utilis.css';
-import '../styles/home.css';
 import '../styles/header.css';
 import '../styles/mainSection.css';
 import '../styles/bottomNav.css';
