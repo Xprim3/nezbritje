92d399f88967c27407869e3d2b241c6531eeda34
Denis Januzi <denisjanuzi@gmail.com>
2024-12-15 16:00:37 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,22 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        cart: resolve(__dirname, 'cart.html'),
-        // Add uicomponents.js as part of your input
-        uicomponents: resolve(__dirname, 'src/uicomponents.js'), // Entry for your JS components
+        main: resolve(__dirname, 'index.html'),           // Main entry file
+        cart: resolve(__dirname, 'public/cart.html'),     
+        uicomponents: resolve(__dirname, 'src/uicomponents.js'), // Entry for your UI components JavaScript
       },
+      output: {
+        dir: 'dist',
+      }
     },
   },
   resolve: {
     alias: {
-      '@components': resolve(__dirname, 'src/components'), // You can set an alias for easier imports
+      '@components': resolve(__dirname, 'src/components'), // Alias for easier imports
     },
   },
 })
