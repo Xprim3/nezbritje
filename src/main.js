5a8cd4f710e7640b506658c1add78ba40ce2658d
Denis Januzi <denisjanuzi@gmail.com>
2024-12-16 17:01:52 +0100

    update

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import '../src/components/styles/utilis.css';
 import '../src/components/styles/header.css';
 import '../src/components/styles/mainSection.css';
 import '../src/components/styles/bottomNav.css';
+import '../src/components/styles/footer.css';
 import '../src/components/styles/adminNav.css';
 import '../src/components/styles/controls.css';
 import '../src/components/styles/shopping-cart.css';
