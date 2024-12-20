08ef89ddfcf0149f030e2fe4223057a6fe11dca1
Denis Januzi <denisjanuzi@gmail.com>
2024-12-20 15:37:35 +0100

    update

--- src/components/headerMenu/headerMenu.js
+++ src/components/headerMenu/headerMenu.js
@@ -1,10 +1,17 @@
-window.toggleDropdown = function() {
+window.toggleDropdown = function () {
   const dropdown = document.querySelector('.header-dropdown-btn');
   
   // Toggle the 'show' class to trigger CSS transitions
-  if (dropdown.classList.contains('show')) {
-    dropdown.classList.remove('show');
-  } else {
-    dropdown.classList.add('show');
-  }
+  dropdown.classList.toggle('show');
 };
+
+// Close dropdown when clicking outside
+document.addEventListener('click', function (event) {
+  const dropdown = document.querySelector('.header-dropdown-btn');
+  const button = document.querySelector('button[onclick="toggleDropdown()"]'); // Select the button with the toggle function
+
+  // Check if the click is outside the dropdown and button
+  if (!dropdown.contains(event.target) && !button.contains(event.target)) {
+    dropdown.classList.remove('show');
+  }
+});
